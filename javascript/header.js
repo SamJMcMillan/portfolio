3df9ba06aa0162f00bef4fcb929164097e221ae8
SamJMcMillan <sam.ja.mcmillan@gmail.com>
2022-11-08 15:47:22 +0000

    About Slides and Nav

--- javascript/header.js
+++ javascript/header.js
@@ -1,12 +1,34 @@
-$(document).ready(function () {
-  $(window).on("scroll", function () {
-    if ($(window).scrollTop() < 1000) {
-      $(".hero-wrapper").css(
-        "background-size",
-        85 + parseInt($(window).scrollTop() / 5) + "%"
-      );
-      $(".title").css("top", 50 + $(window).scrollTop() * 0.1 + "%");
-      $(".title").css("opacity", 1 - $(window).scrollTop() * 0.003);
-    }
-  });
+// $(document).ready(function () {
+//   $(window).on("scroll", function () {
+//     if ($(window).scrollTop() < 1000) {
+//       $(".hero-wrapper").css(
+//         "background-size",
+//         85 + parseInt($(window).scrollTop() / 5) + "%"
+//       );
+//       $(".title").css("top", 50 + $(window).scrollTop() * 0.1 + "%");
+//       $(".title").css("opacity", 1 - $(window).scrollTop() * 0.003);
+//     }
+//   });
+// });
+
+var headerParallax = $(".hero-wrapper");
+var elementHeight = headerParallax.outerHeight();
+
+function topParallax() {
+  var scrollPos = $(window).scrollTop();
+  var transformValue = scrollPos / 40;
+  var opacityValue = 1 - scrollPos / 2000;
+  var blurValue = Math.min(scrollPos / 100, 3);
+
+  if (scrollPos < elementHeight) {
+    headerParallax.css({
+      transform: "translate3d(0, -" + transformValue + "%, 0)",
+      opacity: opacityValue,
+      "-webkit-filter": "blur(" + blurValue + "px)",
+    });
+  }
+}
+
+$(window).scroll(function () {
+  topParallax();
 });
