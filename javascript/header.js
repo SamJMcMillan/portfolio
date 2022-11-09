24164ccc175604c9a51ba7f3f7ac32e140641f98
SamJMcMillan <sam.ja.mcmillan@gmail.com>
2022-11-09 10:46:19 +0000

    Hamburger button and parallax updates

--- javascript/header.js
+++ javascript/header.js
@@ -11,24 +11,28 @@
 //   });
 // });
 
-var headerParallax = $(".hero-wrapper");
-var elementHeight = headerParallax.outerHeight();
+// var headerParallax = $(".hero-wrapper");
+// var elementHeight = headerParallax.outerHeight();
 
-function topParallax() {
-  var scrollPos = $(window).scrollTop();
-  var transformValue = scrollPos / 40;
-  var opacityValue = 1 - scrollPos / 2000;
-  var blurValue = Math.min(scrollPos / 100, 3);
+// function topParallax() {
+//   var scrollPos = $(window).scrollTop();
+//   var transformValue = scrollPos / 40;
+//   var opacityValue = 1 - scrollPos / 2000;
+//   var blurValue = Math.min(scrollPos / 100, 3);
 
-  if (scrollPos < elementHeight) {
-    headerParallax.css({
-      transform: "translate3d(0, -" + transformValue + "%, 0)",
-      opacity: opacityValue,
-      "-webkit-filter": "blur(" + blurValue + "px)",
-    });
-  }
-}
+//   if (scrollPos < elementHeight) {
+//     headerParallax.css({
+//       transform: "translate3d(0, -" + transformValue + "%, 0)",
+//       opacity: opacityValue,
+//       "-webkit-filter": "blur(" + blurValue + "px)",
+//     });
+//   }
+// }
+
+// $(window).scroll(function () {
+//   topParallax();
+// });
 
 $(window).scroll(function () {
-  topParallax();
+  $("main").css("background-position", "20% " + $(this).scrollTop() / 2 + "px");
 });
--- javascript/navigation.js
+++ javascript/navigation.js
@@ -22,8 +22,10 @@ $(window).resize(function () {
 function displayNav() {
   if (navLinks.style.display === "none") {
     $(navLinks).slideDown(400);
+    $(navBtn).addClass("is-active");
   } else {
     $(navLinks).slideUp(400);
+    $(navBtn).removeClass("is-active");
   }
 
   return true;
