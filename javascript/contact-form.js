d4f057d4aefad83f7fb753713271d637e0443048
SamJMcMillan <sam.ja.mcmillan@gmail.com>
2022-11-09 11:26:11 +0000

    Regrex validation

--- javascript/contact-form.js
+++ javascript/contact-form.js
@@ -7,6 +7,7 @@ emailPopup.style.display = "none";
 function validateForm() {
   let nameInput = $(".name").val().trim();
   let emailInput = $(".email").val().trim();
+  var emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
   if (nameInput.length === 0) {
     $(namePopup).slideDown(400).delay(2000).slideUp(400);
     return false;
@@ -14,6 +15,9 @@ function validateForm() {
     console.log("names entered");
   }
   if (emailInput.length === 0) {
+    $(emailPopup).slideDown(400).delay(2000).slideUp(400);
+    return false;
+  } else if (emailPattern.test(emailInput) == false) {
     $(emailPopup).slideDown(400).delay(2000).slideUp(400);
     return false;
   } else {
